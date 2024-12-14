af98a86dec4f588a688a859cefcb1154b30046de
Ciloui <143546096+Ciloui@users.noreply.github.com>
2024-12-14 18:05:46 +0100

    chore: simplify build configurations

--- vite.config.ts
+++ vite.config.ts
@@ -6,54 +6,10 @@ dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: [
-      'recharts',
-      'recharts/lib/component/ResponsiveContainer',
-      'recharts/lib/component/LineChart',
-      'recharts/lib/component/Line',
-      'recharts/lib/component/XAxis',
-      'recharts/lib/component/YAxis',
-      'recharts/lib/component/CartesianGrid',
-      'recharts/lib/component/Tooltip',
-      'recharts/lib/component/Legend'
-    ],
-    force: true
-  },
   build: {
-    target: 'es2015',
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser',
     commonjsOptions: {
-      include: [/node_modules/],
       transformMixedEsModules: true
-    },
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      },
-      output: {
-        manualChunks: {
-          vendor: [
-            'react',
-            'react-dom',
-            'recharts',
-            '@tanstack/react-query'
-          ]
-        }
-      }
     }
-  },
-  resolve: {
-    alias: {
-      'recharts': 'recharts/es6'
-    },
-    dedupe: ['react', 'react-dom', 'recharts']
-  },
-  server: {
-    port: 3000,
-    strictPort: true
   }
 });
